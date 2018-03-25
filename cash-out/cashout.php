2198d8d11a2e0f23763c716285607559e6176ae4
BlackBird271 <ian.koide1@icloud.com>
2018-03-24 22:41:59 -0400

    Initial commit

---
+++ cash-out/cashout.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang = 'en-US'>
+<head>
+    <meta charset = 'UTF-8'>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="../img/logo.png" />
+    <title>Cash Out</title>
+    <div id="menu">
+        <nav>
+            <ul>
+                <li><a href="../index.php">CSGO-Boonie</a></li>
+                <li><a href="../earn/earn.php">Earn Coins</a></li>
+                <li><a href="cashout.php">Cash Out</a></li>
+                <li><a href="../flip.php">Flip</a></li>
+                <li><a href="../chat/index.php">Chat</a></li>
+                <li><a href="#mine">Mine</a></li>
+                <li style="float:right"><a class="active" href="../Steam Login/login_steam.php">Login</a></li>
+            </ul>
+        </nav>
+    </div>
+    </head>
+<body>
+	
+	<div class="cobox">
+		<img src="../img/soorigami.jpg" style="height: 90px; width: 100px; margin-top: 9px; margin-left: 15px;">
+		<p>Sawed-Off | Origami</p>
+  	</div>
+  	<div class="cobox">
+		<img src="../img/mtfade.jpg" style="height: 90px; width: 100px; margin-top: 9px; margin-left: 15px;">
+		<p>MAC-10 | Fade</p>
+  	</div>	
+  	<div class="cobox">
+		<img src="../img/m4hb.jpg" style="height: 90px; width: 100px; margin-top: 9px; margin-left: 15px;">
+		<p>M4A1-S | Hyper Beast</p>
+  	</div>		
+</body>
+</html>
